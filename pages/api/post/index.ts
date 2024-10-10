6ad530c1f7bf4dc149017414a79e63ba59da81c7
CirnoTox <zixiaochen50@gmail.com>
2024-10-10 22:20:19 +0900

    Now it is able to create, delete and publish post.

---
+++ pages/api/post/index.ts
@@ -0,0 +1,26 @@
+import { options } from "../auth/[...nextauth]"
+import prisma from '../../../lib/prisma';
+import { getServerSession } from "next-auth/next";
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+  const session = await getServerSession(req, res, options)
+  if (session) {
+    // Signed in
+    console.log("Session", JSON.stringify(session, null, 2));
+    const result = await prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        author: { connect: { email: session?.user?.email } },
+      },
+    });
+    res.json(result);
+  } else {
+    // Not Signed in
+    res.status(401);
+  }
+
+}
