56fc001512e05e9369325239ba9ece4151bd3bc8
CirnoTox <zixiaochen50@gmail.com>
2024-10-15 18:03:00 +0900

    next auth setting update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,6 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import GitHubProvider from 'next-auth/providers/github';
-import prisma from '../../../../lib/prisma';
+import NextAuth from 'next-auth';
+import { authOptions } from '../../../auth-options';
 
-export const authOptions: NextAuthOptions = {
-  // your configs
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
-}
 
 const handler = NextAuth(authOptions)
 
